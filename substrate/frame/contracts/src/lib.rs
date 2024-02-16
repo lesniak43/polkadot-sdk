527688ff52770b17e3e5e7673becd115f3e6d5a3
Marcin <marcin.radecki@cardinals.cc>
2024-02-16 14:13:57 +0100

    Previous approach was a no-op

--- substrate/frame/contracts/src/lib.rs
+++ substrate/frame/contracts/src/lib.rs
@@ -411,13 +411,9 @@ pub mod pallet {
 			use migration::MigrateResult::*;
 
 			loop {
-				let (result, weight) = Migration::<T>::migrate(remaining_weight);
 				const reduced_weight_factor: u64 = 4;
-				let reduced_weight = match weight.checked_mul(4) {
-					Some(weight) => weight,
-					None => weight,
-				};
-				remaining_weight.saturating_reduce(reduced_weight);
+				let (result, weight) = Migration::<T>::migrate(remaining_weight.saturating_div(reduced_weight_factor));
+				remaining_weight.saturating_reduce(weight.saturating_mul(reduced_weight_factor));
 
 				match result {
 					// There is not enough weight to perform a migration, or make any progress, we
